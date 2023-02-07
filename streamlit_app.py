7c2b3d2278c3874da67b43189cd3b162a6846b73
quentinDosInetum <119852576+quentinDosInetum@users.noreply.github.com>
2023-02-07 14:25:00 +0100

    Update streamlit_app.py
    added second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as stl
 
 stl.title('My parents new healthy diner')
+
 stl.header('Breakfast menu')
+
 stl.text('🥣 Omega 3 & Blueberry oatmeal')
 stl.text('🥗 Kale, spinach & rocket smothie')
 stl.text('🐔 Hard-boiled free-range egg')
 stl.text('🥑🍞 Avocado toast')
+
+stl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
