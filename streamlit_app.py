f704cf9808ce4ac783bb2706ddad58e59233a6bc
quentinDosInetum <119852576+quentinDosInetum@users.noreply.github.com>
2023-02-07 14:35:18 +0100

    Update streamlit_app.py
    show only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ stl.text('🥑🍞 Avocado toast')
 stl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-stl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = stl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-stl.dataframe(my_fruit_list)
+stl.dataframe(fruits_to_show)
