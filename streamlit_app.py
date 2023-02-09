3623a3e26086e24a713a58086224efec9b7500ef
quentinDosInetum <119852576+quentinDosInetum@users.noreply.github.com>
2023-02-09 13:36:47 +0100

    Update streamlit_app.py
    fixed requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 stl.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+stl.text(fruityvice_response)
