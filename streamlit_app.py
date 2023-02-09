b438053416e00acc9fe46e93d3e558e06b55ff79
quentinDosInetum <119852576+quentinDosInetum@users.noreply.github.com>
2023-02-09 13:39:22 +0100

    fixed new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 stl.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
+stl.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 stl.text(fruityvice_response.json())
